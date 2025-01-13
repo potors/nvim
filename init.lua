df8189ea99a4ade918c49caa34b20b32c14b8f54
Felipe Pitol Puhl <felpofo@proton.me>
2025-01-13 18:33:57 -0300

    chore: disable thing

---
+++ init.lua
@@ -0,0 +1,14 @@
+-- disable providers
+for _, name in ipairs {
+    'python3', 'perl', 'ruby', 'node',
+} do vim.g['loaded_' .. name .. '_provider'] = 0 end
+
+-- disable plugins
+for _, name in ipairs {
+    'netrw', 'netrwPlugin',
+    'tutor_mode_plugin',
+    'gzip', 'zipPlugin', 'tarPlugin',
+    '2html_plugin', 'sql_completion',
+    'matchit', 'matchparen',
+    'vimball', 'vimballPlugin',
+} do vim.g['loaded_' .. name] = 1 end
